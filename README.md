727e6eba26e36b73bcdf79652678795704b0cc0d
dssyar1k <y.pavlov@inbox.ru>
2025-04-29 22:09:29 +0300

    Первая отправка

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/dssyar1k/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
-// @todo: Темплейт карточки
+//Получение шаблона карточки
+const cardTemplate = document.querySelector("#card-template").content;
+//Контейнер для карточек
+const placesList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+//функция создания карточки
+function createCard(cardContent, deleteCard) {
+  //Клонирование шаблона карточки
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  //Элементы внутри карточки
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  //Установка значений из данных карточки
+  cardTitle.textContent = cardContent.name;
+  cardImage.src = cardContent.link;
+  cardImage.alt = cardContent.name;
+  //Обработчик клика на кнопку
+  deleteButton.addEventListener("click", () => {
+    deleteCard(cardElement);
+  });
+  return cardElement;
+}
 
-// @todo: Функция создания карточки
+//Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+//Вывод карточки на страницу
+initialCards.forEach((cardContent) => {
+  const cardElement = createCard(cardContent, deleteCard);
+  placesList.append(cardElement);
+});
